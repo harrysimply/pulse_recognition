75afa3846fad5acd992cef1b96571e49fd1df773
HuangDaXu <21610138@zju.edu.cn>
2019-03-11 21:06:19 +0800

    训练PulseNet并得到测试样本集的新特征

--- divide_train_test_data.m
+++ divide_train_test_data.m
@@ -1,7 +1,13 @@
-load '.\data\train_data.mat'
+%训练自动编码机PulseNet
+%得到训练输出特征
+
+clear
+load '.\data\train_data_30.mat'
 load '.\data\test_data.mat'
-X=mapminmax(train_data(:,1:100)',0,1);
+X=mapminmax(train_data(:,1:100)',0,1);%归一化
 T=mapminmax(test_data(:,1:100)',0,1);
+
+%三层自动编码机
 hidden_size =80;
 autoenc_1=trainAutoencoder(X,hidden_size,...
     'MaxEpochs',1000,...
@@ -36,15 +42,19 @@ autoenc_3=trainAutoencoder(features_2,hidden_size_3,...
 
 features_3=encode(autoenc_3,features_2);
 
+%三层自动编码机的编码器堆栈
 autoenc=stack(autoenc_1,autoenc_2,autoenc_3);
 view(autoenc);
+
 res_train=autoenc(X);
+
+%将训练集输入堆栈自动编码机也就是PulseNet中得到提取出的特征保存
 res_test=autoenc(T);
-save('.\data\res_test_data.mat','res_test')
-save('.\data\res_train_data.mat','res_train')
+save('.\data\res_test_data_30.mat','res_test')%根据训练好的网络得到测试输出，即新的特征并保存
+save('.\data\res_train_data_30.mat','res_train')
 
 
-
+%特征可视化
 figure(1)
 h=plotWeights(autoenc_1);
 figure(2)
